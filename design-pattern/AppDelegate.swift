9bc352f958b034ccfe25659f516aade85ba5e9ea
niwaguan <niwaguan@gmail.com>
2021-08-25 10:00:17 +0800

    Initial Commit

---
+++ design-pattern/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  design-pattern
+//
+//  Created by Gaoyang on 2021/8/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
